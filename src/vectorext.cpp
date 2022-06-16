f7247a68aa5478566a69a6ead422829e67e0758f
marck283 <86911602+marck283@users.noreply.github.com>
2022-06-17 01:22:06 +0200

    Added warning on 'join' function

--- src/vectorext.cpp
+++ src/vectorext.cpp
@@ -23,6 +23,8 @@ template<class T> std::ostream& operator<<(std::ostream& os, const std::vector<T
  * 
  * @param v1 The first vector
  * @param v2 The second vector
+ * 
+ * @warning As of C++20, there are standard functions that allow two vectors to be joined. This hand-made version is therefore suitable for all C++ standard versions prior to C++20.
  */
 template<class T> void join(std::vector<T>& v1, const std::vector<T>& v2) noexcept {
     for(const auto& i: v2) {
