d03d74328e5e5ab8fc2f8cea7daab8f0df427b72
marck283 <86911602+marck283@users.noreply.github.com>
2022-07-24 18:06:29 +0200

    Sistemata documentazione

--- src/vectorext.cpp
+++ src/vectorext.cpp
@@ -6,11 +6,12 @@
 /**
  * @brief This operator definition prints all the elements inside the given vector.
  * 
- * @param os The stream on whiich the elements are printed.
+ * @param os The stream on which the elements are printed.
  * @param v The vector from which to take the elements.
  * @return std::ostream& The so modified stream.
  * 
  * @warning The parameter class T must define the "<<" operator in order to be printable to the output stream.
+ * @warning This function requires a version of the C++ standard which supports rvalue references.
  */
 template<class T> std::ostream& operator<<(std::ostream& os, const std::vector<T>&& v) {
     std::for_each(v.begin(), v.end(), [&os](const T& i) {
@@ -25,7 +26,8 @@ template<class T> std::ostream& operator<<(std::ostream& os, const std::vector<T
  * @param v1 The first vector
  * @param v2 The second vector
  * 
- * @warning As of C++20, there are standard functions that allow two vectors to be joined. This hand-made version is therefore suitable for all C++ standard versions prior to C++20.
+ * @warning As of C++20, there are standard functions that allow two vectors to be joined. This hand-made version is therefore suitable for all C++ standard versions from C++11 onwards and prior to C++20.
+ * @warning This function requires a version of the C++ standard which supports rvalue references.
  */
 template<class T> void join(std::vector<T>&& v1, const std::vector<T>&& v2) noexcept {
     std::for_each(v2.begin(), v2.end(), [&v1](const T& i) {
@@ -38,7 +40,7 @@ template<class T> void join(std::vector<T>&& v1, const std::vector<T>&& v2) noex
 /**
  * @brief This operator definition prints all the elements inside the given vector.
  * 
- * @param os The stream on whiich the elements are printed.
+ * @param os The stream on which the elements are printed.
  * @param v The vector from which to take the elements.
  * @return std::ostream& The so modified stream.
  * 
